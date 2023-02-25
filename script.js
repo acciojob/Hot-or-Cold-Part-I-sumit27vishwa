6cc99898acc1a37c1cdfc972299d9fa6f7a58046
sumit27vishwa <117379693+sumit27vishwa@users.noreply.github.com>
2023-02-25 21:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here
+var btn = document.getElementById("btn");
+var numPtag = document.getElementById("num");
+var res = document.getElementById("response");
+var inputValue = document.getElementById("guess").value;
 
+
+
+
+function rendomNumGenerator(){
+	rendomNum = Math.floor(Math.random() * 40) - 20;
+	document.getElementById("num").innerText = randomNum;
+	guessNum();
+	print();
+}
+function guessNum(){
+	input = number( documtent.getElementById("guess").value);
+}
+function print() {
+	
+if(Math.abs(randomNum - input) <= 5){
+   document.getElementById("response").innerText = "Hot";
+}
+else(
+   document.getElementById("response").innerText = "Cold";
+)
+	
+}
+btn.addEventListener('click',rendomNumGenerator);
